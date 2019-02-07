1854237f588facb6e083fe5869297768cdc2eba6
Schram <crschram@UMICH.EDU>
2019-02-07 14:35:32 -0500

    committed to stagin ducks

---
+++ duck.java
@@ -0,0 +1,16 @@
+//changes being made
+
+Public class duck {
+  public fly()
+  {
+    system.out.println("A duck can fly");
+  }
+  public swim()
+  {
+    system.out.println("A duck can swim");
+  }
+  public eat()
+  {
+      system.out.println("A duck can eat A LOT");
+  }
+  }
